7f4828713926c0c7aca9782c6ae67a70a15fe122
Aimaral Kairbekova <aimaralkairbekova@mail.ru>
2021-08-28 16:05:53 +0600

    first commit

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        UserAdministrator userAdministrator = new UserAdministrator("Айдар");
+        SupplierReader supplierReader = new SupplierReader("Анель");
+        LibrarianAdministrator librarianAdministrator = new LibrarianAdministrator("Айна");
+
+        userAdministrator.overdueNotification(supplierReader);
+        supplierReader.takeBook(userAdministrator);
+        supplierReader.returnBook(userAdministrator);
+        librarianAdministrator.orderBook(supplierReader);
+
+    }
+}
